c0d1a98c03d7c30e9bedea85ad0c9dd8da31a6f3
ryosuke shimizu(清水 亮介) <RyShimizu@topcon.com>
2020-02-03 17:25:02 +0900

    implement logic. Need to modify to switch color and image using checkbox

--- app/src/main/java/com/example/kotlinfundamental_colormyview/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfundamental_colormyview/MainActivity.kt
@@ -5,14 +5,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.service.autofill.TextValueSanitizer
 import android.view.View
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var checkBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val status = arrayOf(
+            "Color",
+            "Image")
+
+        checkBox = findViewById(R.id.checkbox)
+
+        checkBox.isChecked = false
+        checkBox.text = status[0]
+
+        checkBox.setOnClickListener(View.OnClickListener {
+            val check = checkBox.isChecked
+            if (check) {
+                checkBox.text = status[0]
+            } else {
+                checkBox.text = status[1]
+            }
+        })
 
         setListeners()
     }
@@ -29,6 +51,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun makeResource(view: View) {
+        // When user tap the UI item, the color of UI item will be changed.
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundResource(R.drawable.ic_android_black)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.ic_android_blue)
+            R.id.box_three_text -> view.setBackgroundResource(R.drawable.ic_android_green)
+            R.id.box_four_text -> view.setBackgroundResource(R.drawable.ic_android_purple)
+            R.id.box_five_text -> view.setBackgroundResource(R.drawable.ic_android_red)
+        }
+    }
+
     private fun setListeners() {
         val boxOneText: TextView = findViewById(R.id.box_one_text)
         val boxTwoText: TextView = findViewById(R.id.box_two_text)
@@ -41,8 +74,15 @@ class MainActivity : AppCompatActivity() {
         val clickableViews: List<View> =
             listOf(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, rootConstraintLayout)
 
-        for (item in clickableViews) {
-            item.setOnClickListener { makeColored(it) }
+        val check = checkBox.isChecked
+        if (check) {
+            for (item in clickableViews) {
+                item.setOnClickListener { makeColored(it) }
+            }
+        } else {
+            for (item in clickableViews) {
+                item.setOnClickListener { makeResource(it) }
+            }
         }
     }
 }
