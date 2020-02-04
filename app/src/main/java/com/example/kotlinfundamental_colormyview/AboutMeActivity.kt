14afa6dd4f75879b7919f2e07cedb3ab6987dbaa
Ryosuke Shimizu <r.shimizu18020@gmail.com>
2020-02-05 02:25:06 +0900

    Integrate other fundamental apps

---
+++ app/src/main/java/com/example/kotlinfundamental_colormyview/AboutMeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinfundamental_colormyview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutMeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about_me_activity)
+    }
+}
