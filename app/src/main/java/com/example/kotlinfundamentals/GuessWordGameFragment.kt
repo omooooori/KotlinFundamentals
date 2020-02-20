8d1864ddd5359e6a9e3546d2ca98b5252d43ce08
ora1027 <r.shimizu18020@gmail.com>
2020-02-21 08:35:34 +0900

    Complete GuessWordGame Title layout

--- app/src/main/java/com/example/kotlinfundamentals/GuessWordGameFragment.kt
+++ app/src/main/java/com/example/kotlinfundamentals/GuessWordGameFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import com.example.kotlinfundamentals.databinding.FragmentGameBinding
 import com.example.kotlinfundamentals.databinding.FragmentGuessWordGameBinding
 
 
