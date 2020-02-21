10fef56662f4c492a5e55018549d9083a25d7c7d
ora1027 <r.shimizu18020@gmail.com>
2020-02-21 20:40:15 +0900

    Implement guess word game part

--- app/src/main/java/com/example/kotlinfundamentals/GuessWordGameFragment.kt
+++ app/src/main/java/com/example/kotlinfundamentals/GuessWordGameFragment.kt
@@ -2,21 +2,21 @@ package com.example.kotlinfundamentals
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.example.kotlinfundamentals.databinding.FragmentGuessWordGameBinding
 
 
 class GuessWordGameFragment : Fragment() {
 
-    private var word = ""
-
-    private var score = 0
-
     private lateinit var binding: FragmentGuessWordGameBinding
+
+    private lateinit var viewModel: GameViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,7 +30,34 @@ class GuessWordGameFragment : Fragment() {
             false
         )
 
+        Log.i("GameFragment", "Called ViewModelProvider.of")
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
+
+        binding.correctButton.setOnClickListener { onCorrect() }
+        binding.skipButton.setOnClickListener { onSkip() }
+        updateScoreText()
+        updateWordText()
+
         return binding.root
     }
 
+    private fun onSkip() {
+        viewModel.onSkip()
+        updateWordText()
+        updateScoreText()
+    }
+
+    private fun onCorrect() {
+        viewModel.onCorrect()
+        updateWordText()
+        updateScoreText()
+    }
+
+    private fun updateWordText() {
+        binding.wordText.text = viewModel.word
+    }
+
+    private fun updateScoreText() {
+        binding.scoreText.text = viewModel.score.toString()
+    }
 }
