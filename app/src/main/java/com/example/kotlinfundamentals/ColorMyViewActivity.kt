8236eed9077ee628dfe34a790dc4896546f66ec2
ora1027 <r.shimizu18020@gmail.com>
2020-02-26 06:56:14 +0900

    Move current MainActivity to ColorMyViewActivity

---
+++ app/src/main/java/com/example/kotlinfundamentals/ColorMyViewActivity.kt
@@ -0,0 +1,79 @@
+package com.example.kotlinfundamentals
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ColorMyViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    private fun makeColored(view: View) {
+        // When user tap the UI item, the color of UI item will be changed.
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.button_red -> view.setBackgroundResource(R.color.my_red)
+            R.id.button_yellow -> view.setBackgroundResource(R.color.my_yellow)
+            R.id.button_green -> view.setBackgroundResource(R.color.my_green)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+
+    private fun makeResource(view: View) {
+        // When user tap the UI item, the color of UI item will be changed.
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundResource(R.drawable.ic_android_black)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.ic_android_blue)
+            R.id.box_three_text -> view.setBackgroundResource(R.drawable.ic_android_green)
+            R.id.box_four_text -> view.setBackgroundResource(R.drawable.ic_android_purple)
+            R.id.box_five_text -> view.setBackgroundResource(R.drawable.ic_android_red)
+        }
+    }
+
+    private fun setListeners() {
+        val boxOneText: TextView = findViewById(R.id.box_one_text)
+        val boxTwoText: TextView = findViewById(R.id.box_two_text)
+        val boxThreeText: TextView = findViewById(R.id.box_three_text)
+        val boxFourText: TextView = findViewById(R.id.box_four_text)
+        val boxFiveText: TextView = findViewById(R.id.box_five_text)
+
+        val rootConstraintLayout: ConstraintLayout = findViewById(R.id.constraint_layout)
+
+        val redButton: TextView = findViewById(R.id.button_red)
+        val greenButton: TextView = findViewById(R.id.button_green)
+        val yellowButton: TextView = findViewById(R.id.button_yellow)
+
+        val clickableViews: List<View> =
+            listOf(
+                boxOneText,
+                boxTwoText,
+                boxThreeText,
+                boxFourText,
+                boxFiveText,
+                rootConstraintLayout,
+                redButton,
+                greenButton,
+                yellowButton
+            )
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+
+//        for (item in clickableViews) {
+//            item.setOnClickListener { makeResource(it) }
+//        }
+    }
+}
